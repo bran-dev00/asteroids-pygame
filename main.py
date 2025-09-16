5588ff22308eb46ec1b66be1d94b6b3d8bf78282
Brandon <crandon1327@gmail.com>
2025-09-16 16:04:05 -0500

    added basic wasd movement

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import pygame
 from constants import *
+from player import * 
 
+
+# Base Class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x,y)
+        self.velocity = pygame.Vector2(0,0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
 
 def main():
     pygame.init()
@@ -8,12 +27,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 #Delta-Time
 
+    #Player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT / 2)
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
         dt = clock.tick(60) / 1000
         
 
