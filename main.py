503d82c6668cf1e4de6fffa13c64e0ce36aa3e94
Brandon <crandon1327@gmail.com>
2025-09-16 14:41:26 -0500

    set game clock speeed and delta time

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0 #Delta-Time
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+
+
 
 
 
