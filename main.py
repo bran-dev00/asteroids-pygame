78c90d1866c17ee5ccd91e1e2ea2974df8ac9035
Brandon <crandon1327@gmail.com>
2025-09-16 17:44:29 -0500

    spawned astroids and moved base circle class to it's own file

--- main.py
+++ main.py
@@ -1,51 +1,41 @@
 import pygame
 from constants import *
-from player import * 
-
-
-# Base Class for game objects
-class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-
-        self.position = pygame.Vector2(x,y)
-        self.velocity = pygame.Vector2(0,0)
-        self.radius = radius
-
-    def draw(self, screen):
-        pass
-
-    def update(self, dt):
-        pass
+from asteroid import Asteroid 
+from asteroid_field import AsteroidField
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0 #Delta-Time
 
-    #Player
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT / 2)
+
+    dt = 0 #Delta-Time
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
         
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
